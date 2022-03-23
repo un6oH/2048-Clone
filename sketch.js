b698f3ff1bb7a73067641c71ba7bacb01d81c0d6
un6oy <un6oy.hogun@gmail.com>
2022-03-23 21:05:42 +1100

    Converted syntax to JS

--- sketch.js
+++ sketch.js
@@ -1,19 +1,19 @@
-int score, highscore, occupied;
-boolean gameActive, gameOver;
-int animStage, animTimer;
-String[] data;
+var score, highscore, occupied;
+let gameActive, gameOver;
+var animStage, animTimer;
+var data;
 
-Format format;
-Settings settings;
+var format;
+var settings;
 
-Cell[][] cells = new Cell[4][4];
-Tile[] tiles = new Tile[17];
-IntList newTileIds = new IntList();
-IntList movingTileIds = new IntList();
-IntList poppingTileIds = new IntList();
+const cells = [[]];
+const tiles = [];
+const newTileIds = [];
+const movingTileIds = [];
+const poppingTileIds = [];
 
-void setup(){
-  size(600, 700);
+function setup(){
+  createCanvas(600, 700);
   colorMode(HSB, 360, 100, 100, 100);
   format = new Format(width, height);
   settings = new Settings();
@@ -24,7 +24,7 @@ void setup(){
   loadGame();
 }
 
-void draw() {
+function draw() {
   background(format.bgColour);
   noStroke();
   
@@ -61,13 +61,13 @@ void draw() {
   
   fill(format.boardColour);
   rect(0, 0, format.boardW, format.boardW, format.boardFillet);
-  for (Cell[] row: cells) {
-    for (Cell cell: row) {
+  for (let row of cells) {
+    for (let cell of row) {
       cell.display();
     }
   }
   textFont(format.numbersFont);
-  for (Tile tile: tiles) {
+  for (let tile of tiles) {
     if (tile.active) {
       tile.display();
     }
@@ -87,15 +87,15 @@ void draw() {
   //animations
   if (animStage == 1) {
     if (animTimer > settings.tilePopFrames) {
-      for (int i: movingTileIds) tiles[i].move();
+      for (let i of movingTileIds) tiles[i].move();
     } else {
       endAnim();
     }
     animTimer--;
   } else if (animStage == 2) {
     if (animTimer <= settings.tilePopFrames && animTimer > 0) {
-      for (int i: poppingTileIds) tiles[i].pop();
-      for (int i: newTileIds) tiles[i].appear();
+      for (let i of poppingTileIds) tiles[i].pop();
+      for (let i of newTileIds) tiles[i].appear();
     } else {
       endAnim();
       if (!gameOver) saveGame();
@@ -104,7 +104,7 @@ void draw() {
   }
 }
 
-void gameReset() {
+function gameReset() {
   gameStart();
   spawnTile();
   spawnTile();
@@ -112,13 +112,13 @@ void gameReset() {
   saveStrings(dataPath("data.txt"), data);
 }
 
-void gameStart() {
-  for (int r = 0; r < 4; r++) {
-    for (int c = 0; c < 4; c++) {
+function gameStart() {
+  for (let r = 0; r < 4; r++) {
+    for (let c = 0; c < 4; c++) {
       cells[c][r] = new Cell(c, r);
     }
   }
-  for (int i = 0; i < 17; i++) {
+  for (let i = 0; i < 17; i++) {
     tiles[i] = new Tile(i);
   }
   
@@ -130,36 +130,36 @@ void gameStart() {
   animTimer = settings.tilePopFrames;
 }
 
-void gameOver() {
+function gameOver() {
   gameOver = true;
   data[0] = "0";
   saveStrings(dataPath("data.txt"), data);
 }
 
-void spawnTile() {
-  ArrayList<Index> emptyCells = new ArrayList<Index>();
-  for (Cell[] rows: cells) {
-    for (Cell cell: rows) {
+function spawnTile() {
+  let emptyCells = [];
+  for (let rows of cells) {
+    for (let cell of rows) {
       if (!cell.occupied) {
-        Index index = new Index(cell.c, cell.r);
+        let index = new Index(cell.c, cell.r);
         emptyCells.add(index);
       }
     }
   }
   if (emptyCells.size() == 0) return;
   
-  float range = -20000.0 / (score + 22000.0) + 2.0;
-  float random = random(range);
-  int exponent = floor(random) + 1;
-  Index index = emptyCells.get( floor( random( emptyCells.size() ) ) );
+  let range = -20000.0 / (score + 22000.0) + 2.0;
+  let random = random(range);
+  let exponent = floor(random) + 1;
+  let index = emptyCells.get( floor( random( emptyCells.size() ) ) );
   emptyCells.clear();
   
   createTile(index.c, index.r, exponent);
   //createTile(index.c, index.r, int(random(1, 20)));
 }
 
-void createTile(int c, int r, int exp) {
-  for (Tile tile: tiles) {
+function createTile(c, r, exp) {
+  for (let tile of tiles) {
     if (!tile.active) {
       tile.create(c, r, exp);
       cells[c][r].setTile(tile.id);
@@ -173,10 +173,10 @@ void createTile(int c, int r, int exp) {
   }
 }
 
-void calcMove(int input) {
-  boolean horizontal = (input == LEFT || input == RIGHT) ? true : false;
-  int start, end, inc;
-  if (input == LEFT || input == UP) { start = 1; end = 4; inc = 1; }
+function calcMove(input) {
+  let horizontal = (input == LEFT_ARROW || input == RIGHT_ARROW) ? true : false;
+  let start, end, inc;
+  if (input == LEFT_ARROW || input == UP_ARROW) { start = 1; end = 4; inc = 1; }
   else { start = 2; end = -1; inc = -1; }
   print("gameManager::calcMove() moving ");
   switch (input) {
@@ -188,21 +188,21 @@ void calcMove(int input) {
   
   /** HORIZONTAL **/
   if (horizontal) {
-    for (int r = 0; r < 4; r++) {
+    for (let r = 0; r < 4; r++) {
       println("processing row "+r+' ');
-      for (int c = start; c != end; c += inc) {
-        print("processing cell "+c+' '+r+": ");
-        Cell cell = cells[c][r];
+      for (let c = start; c != end; c += inc) {
+        print("processing cell " + c + ' ' + r + ": ");
+        let cell = cells[c][r];
         
         if (!cell.occupied) {
           println("empty");
           continue;
         }
         
-        Index newIndex = new Index(cell.c, cell.r);
-        boolean move = false;
-        int exponent = cell.exponent;
-        for (int i = c - inc; 0 <= i && i < 4; i -= inc) {
+        let newIndex = new Index(cell.c, cell.r);
+        let move = false;
+        let exponent = cell.exponent;
+        for (let i = c - inc; 0 <= i && i < 4; i -= inc) {
           print("checking cell "+i+' '+r+' ');
           if (cells[i][r].occupied) {
             print("occupied ");
@@ -234,21 +234,21 @@ void calcMove(int input) {
   } 
   /** VERTICAL **/
   else { 
-    for (int c = 0; c < 4; c++) {
+    for (let c = 0; c < 4; c++) {
       println("processing column "+c+' ');
-      for (int r = start; r != end; r += inc) {
+      for (let r = start; r != end; r += inc) {
         print("processing cell "+c+' '+r+": ");
-        Cell cell = cells[c][r];
+        let cell = cells[c][r];
         
         if (!cell.occupied) {
           println("empty");
           continue;
         }
         
-        Index newIndex = new Index(cell.c, cell.r);
-        boolean move = false;
-        int exponent = cell.exponent;
-        for (int i = r - inc; 0 <= i && i < 4; i -= inc) {
+        let newIndex = new Index(cell.c, cell.r);
+        let move = false;
+        let exponent = cell.exponent;
+        for (let i = r - inc; 0 <= i && i < 4; i -= inc) {
           print("checking cell "+c+' '+i+' ');
           if (cells[c][i].occupied) {
             print("occupied ");
@@ -281,15 +281,15 @@ void calcMove(int input) {
   if (score > highscore) highscore = score;
 }
 
-void startAnim() {
+function startAnim() {
   animStage = 1;
   animTimer = settings.animFrames;
 }
 
-void endAnim() {
+function endAnim() {
   if (animStage == 1) {
-    for (Cell[] row: cells) {
-      for (Cell cell: row) {
+    for (let row of cells) {
+      for (let cell of row) {
         if (cell.occupied) {
           cell.update();
           occupied++;
@@ -302,20 +302,20 @@ void endAnim() {
     }
     occupied = 0;
     
-    for (int i: movingTileIds) {
+    for (let i of movingTileIds) {
       tiles[i].pos.set(tiles[i].targetPos);
     }
     movingTileIds.clear();
-    for (int i: poppingTileIds) {
+    for (let i of poppingTileIds) {
       tiles[i].scaleF = format.tilePopF;
     }
     
     animStage = 2;
   } else {
-    for (int i: poppingTileIds) {
+    for (let i of poppingTileIds) {
       tiles[i].scaleF = 1;
     }
-    for (int i: newTileIds) {
+    for (let i of newTileIds) {
       tiles[i].scaleF = 1;
     }
     poppingTileIds.clear();
@@ -324,13 +324,13 @@ void endAnim() {
   }
 }
 
-void checkLoss() {
+function checkLoss() {
   print("checkLoss() ");
-  boolean movesAvailable = false;
-  for (int i = 0; i < 4; i++) {
-    int rexp = cells[i][0].exponent, cexp = cells[0][i].exponent;
-    for (int j = 1; j < 4; j++) {
-      int crexp = cells[i][j].exponent, ccexp = cells[j][i].exponent;
+  let movesAvailable = false;
+  for (let i = 0; i < 4; i++) {
+    let rexp = cells[i][0].exponent, cexp = cells[0][i].exponent;
+    for (let j = 1; j < 4; j++) {
+      let crexp = cells[i][j].exponent, ccexp = cells[j][i].exponent;
       if (crexp == rexp || ccexp == cexp) {
         movesAvailable = true;
         print("move available ");
@@ -347,61 +347,14 @@ void checkLoss() {
   print('\n');
 }
 
-void loadGame() {
-  data = loadStrings("data.txt");
-  highscore = Integer.parseInt(data[1]);
-  if (int(data[0]) == 0) {
-    spawnTile();
-    spawnTile();
-    return;
-  }
-  score = Integer.parseInt(data[2]);
-  for (int r = 0; r < 4; r++) {
-    String[] row = split(data[r+3], ';');
-    for (int c = 0; c < 4; c++) {
-      String[] cellData = split(row[c], ',');
-      int exponent = Integer.parseInt(cellData[0]);
-      if (exponent != 0) {
-        int tileId = Integer.parseInt(cellData[1]);
-        tiles[tileId].create(c, r, exponent);
-        cells[c][r].exponent = exponent;
-        cells[c][r].setTile(tileId);
-        cells[c][r].update();
-        newTileIds.append(tileId);
-      }
-    }
-  }
-}
-
-void saveGame() {
-  data[0] = "1";
-  data[1] = str(highscore);
-  data[2] = str(score);
-  for (int r = 0; r < 4; r++) {
-    String row = "";
-    for (int c = 0; c < 4; c++) {
-      row += cells[c][r].exponent + "," + cells[c][r].tileId;
-      if (c < 3) row += ";";
-    }
-    data[r+3] = row;
-  }
-  
-  try {
-    saveStrings(dataPath("data.txt"), data);
-  } catch (NullPointerException e) {
-    println("saveGame() saveStrings failed");
-  }
-  println("saveGame()");
-}
-
-void mouseClicked() {
+function mouseClicked() {
   //spawnTile();
   if (mouseX > format.newGameButtonPos.x - format.newGameButtonSize.x / 2 && mouseX < format.newGameButtonPos.x + format.newGameButtonSize.x  / 2 && mouseY > format.newGameButtonPos.y - format.newGameButtonSize.y / 2 && mouseY < format.newGameButtonPos.y + format.newGameButtonSize.y / 2) {
     gameReset();
   }
 }
 
-void keyPressed() {
+function keyPressed() {
   if (!gameOver && (keyCode == UP || keyCode == DOWN || keyCode == RIGHT || keyCode == LEFT)) {
     while (animStage > 0) endAnim();
     startAnim();
@@ -410,199 +363,180 @@ void keyPressed() {
 }
 
 class Format {
-  int w, h;
-  float boardMargin, boardW, cellMargin, tileW, cellSpacing, boardFillet, tileFillet;
-  
-  PFont titleFont, numbersFont, scoreFont, gameOverTextFont;
-  float numbersFontOffset;
-  color titleColour = color(0, 0, 100);  
-  color menuColour = color(0, 0, 100);
-  color bgColour = color(0, 0, 0);
-  color boardColour = color(0, 0, 50);
-  color cellColour = color(0, 0, 90);
-  color[] tileColours = new color[20];
-  color numColour = color(0);
-  
-  float tilePopF;
-  float tileStartF = 0.2;
-  
-  PVector newGameButtonPos, newGameButtonSize;
-  PVector saveGameButtonPos, saveGameButtonSize;
-  PVector scoreBoxPos;
-  PVector highscoreBoxPos;
-  PVector scoreBoxSize;
-  
-  float scoreTextSize;
-  
-  Format (int w_, int h_) {
-    w = w_;
-    h = h_;
+  constructor (w_, h_) {
+    this.w = w_;
+    this.h = h_;
     
-    boardW = w * 0.9;
-    boardMargin = (w - boardW) / 2;
-    cellMargin = boardW * 0.025;
-    tileW = (boardW - cellMargin * 5) / 4;
-    cellSpacing = cellMargin + tileW;
+    this.boardW = this.w * 0.9;
+    this.boardMargin = (this.w - this.boardW) / 2;
+    this.cellMargin = this.boardW * 0.025;
+    this.tileW = (this.boardW - this.cellMargin * 5) / 4;
+    this.cellSpacing = this.cellMargin + this.tileW;
     
-    newGameButtonPos = new PVector(w - boardMargin - cellMargin - tileW / 2, boardMargin + tileW * 5 / 8);
-    newGameButtonSize = new PVector(tileW, tileW / 4);
-    scoreBoxPos = new PVector(w - boardMargin - cellMargin - tileW / 2, boardMargin + tileW /4 - 10);
-    highscoreBoxPos = new PVector(w - boardMargin - cellMargin * 2 - tileW * 1.5, boardMargin + tileW /4 - 10);
-    scoreBoxSize = new PVector(tileW, tileW / 2);
+    this.newGameButtonPos = createVector(this.w - this.boardMargin - this.cellMargin - this.tileW / 2, this.boardMargin + this.tileW * 5 / 8);
+    this.newGameButtonSize = createVector(this.tileW, this.tileW / 4);
+    this.scoreBoxPos = createVector(this.w - this.boardMargin - this.cellMargin - this.tileW / 2, this.boardMargin + this.tileW /4 - 10);
+    this.highscoreBoxPos = createVector(this.w - this.boardMargin - this.cellMargin * 2 - this.tileW * 1.5, this.boardMargin + this.tileW /4 - 10);
+    this.scoreBoxSize = createVector(this.tileW, this.tileW / 2);
     
-    boardFillet = boardMargin / 4;
-    tileFillet = boardFillet;
+    this.boardFillet = this.boardMargin / 4;
+    this.tileFillet = this.boardFillet;
     
-    tilePopF = (tileW + cellMargin * 2) / tileW;
+    this.tilePopF = (this.tileW + this.cellMargin * 2) / this.tileW;
     
-    titleFont = createFont("SansSerif.bold", tileW);
-    numbersFont = createFont("SansSerif.bold", tileW/2);
-    numbersFontOffset = tileW/20;
-    scoreFont = createFont("SansSerif.bold", 20);
-    gameOverTextFont = createFont("SansSerif.bold", tileW / 2);
+    this.titleFont = loadFont("SansSerif.bold", this.tileW);
+    this.numbersFont = loadFont("SansSerif.bold", this.tileW / 2);
+    this.numbersFontOffset = this.tileW / 20;
+    this.scoreFont = loadFont("SansSerif.bold", 20);
+    this.gameOverTextFont = loadFont("SansSerif.bold", tileW / 2);
     
-    scoreTextSize = scoreFont.getSize();
+    this.scoreTextSize = this.scoreFont.getSize();
 
-    for (int i = 0; i < 10; i++) tileColours[i] = color(60 + 30 * i, 25, 100);
-    for (int i = 0; i < 10; i++) tileColours[i+10] = color(30 * i, 50, 100);
+    this.tileColours = [];
+    for (let i = 0; i < 10; i++) this.tileColours[i] = color(60 + 30 * i, 25, 100);
+    for (let i = 0; i < 10; i++) this.tileColours[i+10] = color(30 * i, 50, 100);
   }
 }
 
 class Settings {
-  int moveFrames = 10;
-  int tilePopFrames = 5;
-  int appearFrames = 5;
-  int animFrames = moveFrames + tilePopFrames;
-  int highscore = 0;
+  constructor() {
+    this.moveFrames = 10;
+    this.tilePopFrames = 5;
+    this.appearFrames = 5;
+    this.animFrames = this.moveFrames + this.tilePopFrames;
+    this.highscore = 0;
+  }
 }
 
 class Cell {
-  int c, r, exponent, tileId, newTileId;
-  PVector pos;
-  boolean occupied = false, isCombining = false;
-  
-  Cell(int x_, int y_) {
-    c = x_;
-    r = y_;
-    pos = new PVector(format.cellMargin + (format.cellSpacing) * c, format.cellMargin + (format.cellSpacing) * r);
+  constructor(c, r) {
+    this.c = c;
+    this.r = r;
+    this.exponent;
+    this.tileId;
+    this.newTileId;
+    this.occupied = false;
+    this.isCombining = false;
+    this.pos = createVector(format.cellMargin + (format.cellSpacing) * this.c, format.cellMargin + (format.cellSpacing) * this.r);
   }
   
-  void display() {
+  display() {
     fill(format.cellColour);
-    rect(pos.x, pos.y, format.tileW, format.tileW, format.tileFillet);
+    rect(this.pos.x, this.pos.y, format.tileW, format.tileW, this.format.tileFillet);
     //if (occupied) {
     //  fill(0);
     //  text(int(pow(2, exponent)), pos.x + format.tileW / 2, pos.y + format.tileW / 2 + format.numbersFont.getSize() * 0.4);
     //}
   }
   
-  void empty() {
-    occupied = false;
-    exponent = 0;
+  empty() {
+    this.occupied = false;
+    this.exponent = 0;
   }
   
-  void setTile(int id) {
-    if (occupied) {
-      newTileId = id;
-      isCombining = true;
+  setTile(id) {
+    if (this.occupied) {
+      this.newTileId = id;
+      this.isCombining = true;
     } else {
-      occupied = true;
-      tileId = id;
+      this.occupied = true;
+      this.tileId = id;
     }
   }
   
-  void update() {
+  update() {
     printId();
     print("update() ");
-    print("exponent="+exponent+' ');
-    if (isCombining) {
-      tiles[newTileId].destroy();
-      print("destroyed tile "+newTileId+' ');
-      poppingTileIds.append(tileId);
-      isCombining = false;
+    print("exponent=" + this.exponent + ' ');
+    if (this.isCombining) {
+      tiles[this.newTileId].destroy();
+      print("destroyed tile " + this.newTileId + ' ');
+      poppingTileIds.append(this.tileId);
+      this.isCombining = false;
     }
-    tiles[tileId].setExp(exponent);
-    print("tileId="+tileId);
-    print('\n');
+    tiles[this.tileId].setExp(this.exponent);
+    println("tileId=" + this.tileId);
   }
   
-  void printId() {
-    print("cells["+c+"]["+r+"]::");
+  printId() {
+    print("cells[" + this.c + "][" + this.r + "]::");
   }
 }
 
 class Index {
-  int c, r;
-  Index(int x_, int y_) {
-    c = x_;
-    r = y_;
+  constructor(c, r) {
+    this.c = c;
+    this.r = r;
   }
 }
 
 class Tile {
-  int id, exponent;
-  boolean active = false;
-  PVector pos, targetPos, vel;
-  float scaleF, textSize;
-  Tile (int i) {
-    id = i;
+  constructor (i) {
+    this.id = i;
+    this.exponent;
+    this.active = false;
+    this.pos = createVector(0, 0);
+    this.targetPos = createVector(0, 0);
+    this.vel = createVector(0, 0);
+    this.scaleF;
+    this.textSize;
   }
   
-  void create(int c, int r, int exp) {
-    pos = new PVector(format.cellMargin + (format.cellSpacing) * c + format.tileW / 2, format.cellMargin + (format.cellSpacing) * r + format.tileW / 2);
-    targetPos = new PVector(pos.x, pos.y);
-    vel = new PVector(0, 0);
+  create(c, r, exp) {
+    this.pos = createVector(format.cellMargin + (format.cellSpacing) * this.c + format.tileW / 2, format.cellMargin + (format.cellSpacing) * this.r + format.tileW / 2);
+    this.targetPos = createVector(this.pos.x, this.pos.y);
+    this.vel = createVector(0, 0);
     setExp(exp);
-    scaleF = format.tileStartF;
-    active = true;
+    this.scaleF = format.tileStartF;
+    this.active = true;
   }
   
-  void destroy() {
-    active = false;
+  destroy() {
+    this.active = false;
   }
   
-  void display() {
+  display() {
     pushMatrix();
-    translate(pos.x, pos.y);
-    scale(scaleF);
+    translate(this.pos.x, this.pos.y);
+    scale(this.scaleF);
     
     fill(format.tileColours[exponent-1]);
     rect(-format.tileW / 2, -format.tileW / 2, format.tileW, format.tileW, format.tileFillet);
     
-    textSize(textSize);
+    textSize(this.textSize);
     fill(format.numColour);
-    text(int(pow(2, exponent)), -2, textSize * 0.4);
+    text(int(pow(2, this.exponent)), -2, this.textSize * 0.4);
     
     popMatrix();
   }
   
-  void goTo(int c, int r) {
-    print("transferring tile "+id+" to "+c+" "+r+' ');
-    targetPos.set(cells[c][r].pos).add(format.tileW/2, format.tileW/2);
-    vel.set(targetPos).sub(pos).div(settings.moveFrames);
-    movingTileIds.append(id);
-    cells[c][r].setTile(id);
+  goTo(c, r) {
+    print("transferring tile " + this.id + " to " + this.c + " " + this.r + ' ');
+    this.targetPos.set(cells[c][r].pos).add(format.tileW/2, format.tileW/2);
+    this.vel.set(this.targetPos).sub(this.pos).div(settings.moveFrames);
+    movingTileIds.append(this.id);
+    cells[c][r].setTile(this.id);
   }
   
-  void setExp(int exp) {
-    exponent = exp;
+  setExp(exp) {
+    this.exponent = exp;
     
     textSize = format.numbersFont.getSize();
-    if (exponent > 6) {
-      int log = int(log(pow(2, exp)) / log(10)) + 1;
+    if (this.exponent > 6) {
+      let log = int(log(pow(2, this.exp)) / log(10)) + 1;
       textSize -= format.numbersFontOffset * (log - 1);
     }
   }
   
-  void appear() {
-     scaleF += (1f - format.tileStartF) / settings.appearFrames;
+  appear() {
+    this.scaleF += (1 - format.tileStartF) / settings.appearFrames;
   }
   
-  void move() {
-    pos.add(vel);
+  move() {
+    this.pos.add(this.vel);
   }
   
-  void pop() {
-    scaleF -= (format.tilePopF - 1) / settings.tilePopFrames;
+  pop() {
+    this.scaleF -= (format.tilePopF - 1) / settings.tilePopFrames;
   }
 }

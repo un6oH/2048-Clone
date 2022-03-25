555ebd02ff824083562c382807d4bcb9dcde907a
un6oy <un6oy.hogun@gmail.com>
2022-03-25 23:07:35 +1100

    Fully functional

--- sketch.js
+++ sketch.js
@@ -1,9 +1,8 @@
 // Clone of 2048 by Gabrielle Cirulli
 
-var score, highscore, occupied;
+var score, highscore = 1337, occupied;
 var gameActive, gameIsOver;
 var animStage, animTimer;
-var data;
 
 const ui = {};
 const settings = {};
@@ -14,15 +13,19 @@ const newTileIds = [];
 const movingTileIds = [];
 const poppingTileIds = [];
 
+function preload() {
+  randomSeed();
+}
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
 
+  textFont(loadFont("data/ARIALBD.TTF"));
   colorMode(HSB, 360, 100, 100, 100);
-  textFont(loadFont("ARIALBD.TTF", 72));
-  setUI(width, height);
+  initUI();
   initSettings();
   
-  gameStart();
+  gameReset();
 
   ui.tileColours = [];
   for (let i = 0; i < 10; i++) { ui.tileColours.push(color(60 + 30 * i, 25, 100)); }
@@ -30,15 +33,20 @@ function setup(){
 }
 
 function draw() {
-  background(ui.bgColour);
+  background(ui.windowColour);
+  push();
+  translate(ui.windowPaddingX, ui.windowPaddingY);
+  scale(ui.scaleF);
   noStroke();
   
+  fill(ui.bgColour);
+  rect(0, 0, ui.w, ui.h);
   //title
   // textFont(ui.font, ui.titleTextSize);
   textSize(ui.titleTextSize);
   fill(ui.titleColour);
-  textAlign(CORNER);
-  text("2048", ui.boardMargin + ui.cellMargin - 10, ui.titleTextSize - 10);
+  textAlign(LEFT);
+  text("2048", ui.gameBoardMargin + ui.cellMargin - 10, ui.titleTextSize - 10);
   
   //scores
   fill(ui.cellColour);
@@ -46,11 +54,11 @@ function draw() {
   // textFont(ui.font, ui.scoreTextSize);
   textSize(ui.scoreTextSize);
   
-  rect(ui.newGameButtonPos.x, ui.newGameButtonPos.y, ui.newGameButtonSize.x, ui.newGameButtonSize.y, ui.tileFillet);
-  rect(ui.scoreBoxPos.x, ui.scoreBoxPos.y, ui.scoreBoxSize.x, ui.scoreBoxSize.y, ui.tileFillet);
-  rect(ui.highscoreBoxPos.x, ui.highscoreBoxPos.y, ui.scoreBoxSize.x, ui.scoreBoxSize.y, ui.tileFillet);
-  
   // buttons
+  rect(ui.newGameButtonPos.x, ui.newGameButtonPos.y, ui.newGameButtonSize.x, ui.newGameButtonSize.y, ui.tileCornerRadius);
+  rect(ui.scoreBoxPos.x, ui.scoreBoxPos.y, ui.scoreBoxSize.x, ui.scoreBoxSize.y, ui.tileCornerRadius);
+  rect(ui.highscoreBoxPos.x, ui.highscoreBoxPos.y, ui.scoreBoxSize.x, ui.scoreBoxSize.y, ui.tileCornerRadius);
+  
   fill(0);
   textAlign(CENTER);
   text("New Game", ui.newGameButtonPos.x, ui.newGameButtonPos.y + 7);
@@ -59,15 +67,16 @@ function draw() {
   text("Best", ui.highscoreBoxPos.x, ui.highscoreBoxPos.y - 5);
   text(highscore, ui.highscoreBoxPos.x, ui.highscoreBoxPos.y + 20);
   
-  //board
+  // game board
   push();
-  translate(ui.boardMargin, height - ui.boardW - ui.boardMargin);
+  translate(ui.gameBoardMargin, ui.h - ui.gameBoardW - ui.gameBoardMargin);
   textAlign(CENTER);
   rectMode(CORNER);
   
-  fill(ui.boardColour);
-  rect(0, 0, ui.boardW, ui.boardW, ui.boardFillet);
+  fill(ui.gameBoardColour);
+  rect(0, 0, ui.gameBoardW, ui.gameBoardW, ui.gameBoardFillet);
   for (let row of cells) {
+    fill(ui.cellColour);
     for (let cell of row) {
       cell.display();
     }
@@ -82,12 +91,12 @@ function draw() {
   
   if (gameIsOver) {
     fill(0, 0, 0, 50);
-    rect(0, 0, ui.boardW, ui.boardW, ui.boardFillet);
+    rect(0, 0, ui.gameBoardW, ui.gameBoardW, ui.gameBoardFillet);
     
     fill(0, 0, 100, 100);
     // textFont(ui.font, ui.gameOverTextSize);
     textSize(ui.gameOverTextSize);
-    text("Game Over", ui.boardW / 2, ui.boardW / 2 - ui.tileW / 5);
+    text("Game Over", ui.gameBoardW / 2, ui.gameBoardW / 2 - ui.tileW / 5);
   }
   
   pop();
@@ -109,21 +118,25 @@ function draw() {
     }
     animTimer--;
   }
+
+  pop();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
-  setUI(width, height);
+  scaleUI(width, height);
 }
 
 function gameReset() {
+  console.log("\n<GAME RESET>")
   gameStart();
   spawnTile();
   spawnTile();
-  data[0] = "0";
 }
 
 function gameStart() {
+  cells.splice(0, 4);
+  tiles.splice(0, 17);
   for (let c = 0; c < 4; c++) {
     cells.push(new Array());
     for (let r = 0; r < 4; r++) {
@@ -145,8 +158,6 @@ function gameStart() {
 
 function gameOver() {
   gameIsOver = true;
-  cells.splice(0, 4);
-  tiles.splice(0, 17);
 }
 
 function spawnTile() {
@@ -155,16 +166,16 @@ function spawnTile() {
     for (let cell of rows) {
       if (!cell.occupied) {
         let index = new Index(cell.c, cell.r);
-        emptyCells.add(index);
+        emptyCells.push(index);
       }
     }
   }
   if (emptyCells.length == 0) return;
   
   let range = -20000.0 / (score + 22000.0) + 2.0;
-  let random = random(range);
-  let exponent = floor(random) + 1;
-  let index = emptyCells.get( floor( random( emptyCells.length ) ) );
+  let randomNum = range * random();
+  let exponent = floor(randomNum) + 1;
+  let index = emptyCells[floor(emptyCells.length * random())];
   emptyCells.splice(0, emptyCells.length);
   
   createTile(index.c, index.r, exponent);
@@ -177,10 +188,10 @@ function createTile(c, r, exp) {
       tile.create(c, r, exp);
       cells[c][r].setTile(tile.id);
       cells[c][r].exponent = exp;
-      print("createTile() ");
+      console.log("createTile() ");
       cells[c][r].update();
       occupied++;
-      newTileIds.append(tile.id);
+      newTileIds.push(tile.id);
       break;
     }
   }
@@ -191,24 +202,24 @@ function calcMove(input) {
   let start, end, inc;
   if (input == LEFT_ARROW || input == UP_ARROW) { start = 1; end = 4; inc = 1; }
   else { start = 2; end = -1; inc = -1; }
-  print("gameManager::calcMove() moving ");
+  console.log("gameManager::calcMove() moving ");
   switch (input) {
-    case LEFT: println("left"); break;
-    case UP: println("up"); break;
-    case RIGHT: println("right"); break;
-    case DOWN: println("down"); break;
+    case LEFT_ARROW: console.log("left"); break;
+    case UP_ARROW: console.log("up"); break;
+    case RIGHT_ARROW: console.log("right"); break;
+    case DOWN_ARROW: console.log("down"); break;
   }
   
   /** HORIZONTAL **/
   if (horizontal) {
     for (let r = 0; r < 4; r++) {
-      println("processing row "+r+' ');
+      console.log("processing row "+r+' ');
       for (let c = start; c != end; c += inc) {
-        print("processing cell " + c + ' ' + r + ": ");
+        console.log("processing cell " + c + ' ' + r + ": ");
         let cell = cells[c][r];
         
         if (!cell.occupied) {
-          println("empty");
+          console.log("empty");
           continue;
         }
         
@@ -216,21 +227,21 @@ function calcMove(input) {
         let move = false;
         let exponent = cell.exponent;
         for (let i = c - inc; 0 <= i && i < 4; i -= inc) {
-          print("checking cell "+i+' '+r+' ');
+          console.log("checking cell "+i+' '+r+' ');
           if (cells[i][r].occupied) {
-            print("occupied ");
+            console.log("occupied ");
             if (cells[i][r].exponent == cell.exponent && !cells[i][r].isCombining) {
-              print("equal ");
+              console.log("equal ");
               newIndex.c = i;
               move = true;
               exponent++;
               score += pow(2, exponent);
-              print("combining to exponent "+exponent+' ');
+              console.log("combining to exponent "+exponent+' ');
               cells[newIndex.c][newIndex.r].isCombining = true;
             }
             break;
           } else {
-            print("empty ");
+            console.log("empty ");
             newIndex.c = i;
             move = true;
           }
@@ -240,21 +251,19 @@ function calcMove(input) {
           cells[c][r].empty();
           tiles[cell.tileId].goTo(newIndex.c, newIndex.r);
         }
-        
-        print('\n');
       }
     }
   } 
   /** VERTICAL **/
   else { 
     for (let c = 0; c < 4; c++) {
-      println("processing column "+c+' ');
+      console.log("processing column "+c+' ');
       for (let r = start; r != end; r += inc) {
-        print("processing cell "+c+' '+r+": ");
+        console.log("processing cell "+c+' '+r+": ");
         let cell = cells[c][r];
         
         if (!cell.occupied) {
-          println("empty");
+          console.log("empty");
           continue;
         }
         
@@ -262,21 +271,21 @@ function calcMove(input) {
         let move = false;
         let exponent = cell.exponent;
         for (let i = r - inc; 0 <= i && i < 4; i -= inc) {
-          print("checking cell "+c+' '+i+' ');
+          console.log("checking cell "+c+' '+i+' ');
           if (cells[c][i].occupied) {
-            print("occupied ");
+            console.log("occupied ");
             if (cells[c][i].exponent == cell.exponent && !cells[c][i].isCombining) {
-              print("equal ");
+              console.log("equal ");
               newIndex.r = i;
               move = true;
               exponent++;
               score += pow(2, exponent);
-              print("combining to exponent "+exponent+' ');
+              console.log("combining to exponent "+exponent+' ');
               cells[newIndex.c][newIndex.r].setTile(cell.tileId);
             }
             break;
           } else {
-            print("empty ");
+            console.log("empty ");
             newIndex.r = i;
             move = true;
           }
@@ -286,8 +295,6 @@ function calcMove(input) {
           cells[c][r].empty();
           tiles[cell.tileId].goTo(newIndex.c, newIndex.r);
         }
-        
-        print('\n');
       }
     }
   }
@@ -338,7 +345,7 @@ function endAnim() {
 }
 
 function checkLoss() {
-  print("checkLoss() ");
+  console.log("checkLoss() ");
   let movesAvailable = false;
   for (let i = 0; i < 4; i++) {
     let rexp = cells[i][0].exponent, cexp = cells[0][i].exponent;
@@ -346,7 +353,7 @@ function checkLoss() {
       let crexp = cells[i][j].exponent, ccexp = cells[j][i].exponent;
       if (crexp == rexp || ccexp == cexp) {
         movesAvailable = true;
-        print("move available ");
+        console.log("move available ");
         break;
       }
       rexp = crexp;
@@ -355,65 +362,72 @@ function checkLoss() {
   }
   if (!movesAvailable) {
     gameOver();
-    print("no moves available; game over");
+    console.log("no moves available; game over");
   }
-  print('\n');
 }
 
 function mouseClicked() {
   //spawnTile();
-  if (mouseX > ui.newGameButtonPos.x - ui.newGameButtonSize.x / 2 && mouseX < ui.newGameButtonPos.x + ui.newGameButtonSize.x  / 2 && mouseY > ui.newGameButtonPos.y - ui.newGameButtonSize.y / 2 && mouseY < ui.newGameButtonPos.y + ui.newGameButtonSize.y / 2) {
+  if (mouseX - ui.windowPaddingX > (ui.newGameButtonPos.x - ui.newGameButtonSize.x / 2) * ui.scaleF && mouseX - ui.windowPaddingX < (ui.newGameButtonPos.x + ui.newGameButtonSize.x  / 2) * ui.scaleF && mouseY - ui.windowPaddingY > (ui.newGameButtonPos.y - ui.newGameButtonSize.y / 2) * ui.scaleF && mouseY - ui.windowPaddingY < (ui.newGameButtonPos.y + ui.newGameButtonSize.y / 2) * ui.scaleF) {
     gameReset();
   }
 }
 
 function keyPressed() {
-  if (!gameIsOver && (keyCode == UP || keyCode == DOWN || keyCode == RIGHT || keyCode == LEFT)) {
+  if (!gameIsOver && (keyCode == UP_ARROW || keyCode == DOWN_ARROW || keyCode == RIGHT_ARROW || keyCode == LEFT_ARROW)) {
     while (animStage > 0) endAnim();
     startAnim();
     calcMove(keyCode);
   }
 }
 
-function setUI(w, h) {
+function scaleUI(w, h) {
   if (w / h <= 6 / 7) {
-    ui.w = w;
-    ui.h = ui.w * float(7 / 6)
+    ui.scaleF = w / ui.w
   } else {
-    ui.h = h;
-    ui.w = ui.h * float(6 / 7);
+    ui.scaleF = h / ui.h
   }
   
-  ui.boardW = ui.w * 0.9;
-  ui.boardMargin = (ui.w - ui.boardW) / 2;
-  ui.cellMargin = ui.boardW * 0.025;
-  ui.tileW = (ui.boardW - ui.cellMargin * 5) / 4;
-  ui.cellSpacing = ui.cellMargin + ui.tileW;
+  ui.windowPaddingX = (w - ui.w * ui.scaleF) / 2;
+  ui.windowPaddingY = (h - ui.h * ui.scaleF) / 2;
+}
+
+function initUI() {
+  ui.w = 600;
+  ui.h = 700;
+  scaleUI(windowWidth, windowHeight);
   
-  ui.newGameButtonPos = createVector(ui.w - ui.boardMargin - ui.cellMargin - ui.tileW / 2, ui.boardMargin + ui.tileW * 5 / 8);
+  ui.gameBoardW = 540;
+  ui.gameBoardMargin = 30;
+  ui.cellMargin = 12;
+  ui.tileW = 120;
+  ui.cellSpacing = 132;
+  
+  ui.newGameButtonPos = createVector(ui.w - ui.gameBoardMargin - ui.cellMargin - ui.tileW / 2, ui.gameBoardMargin + ui.tileW * 5 / 8);
   ui.newGameButtonSize = createVector(ui.tileW, ui.tileW / 4);
-  ui.scoreBoxPos = createVector(ui.w - ui.boardMargin - ui.cellMargin - ui.tileW / 2, ui.boardMargin + ui.tileW /4 - 10);
-  ui.highscoreBoxPos = createVector(ui.w - ui.boardMargin - ui.cellMargin * 2 - ui.tileW * 1.5, ui.boardMargin + ui.tileW /4 - 10);
+  ui.scoreBoxPos = createVector(ui.w - ui.gameBoardMargin - ui.cellMargin - ui.tileW / 2, ui.gameBoardMargin + ui.tileW / 4 - 10);
+  ui.highscoreBoxPos = createVector(ui.w - ui.gameBoardMargin - ui.cellMargin * 2 - ui.tileW * 1.5, ui.gameBoardMargin + ui.tileW / 4 - 10);
   ui.scoreBoxSize = createVector(ui.tileW, ui.tileW / 2);
   
-  ui.boardFillet = ui.boardMargin / 4;
-  ui.tileFillet = ui.boardFillet;
+  ui.gameBoardFillet = ui.gameBoardMargin / 4;
+  ui.tileCornerRadius = ui.gameBoardFillet;
   
   ui.tilePopF = (ui.tileW + ui.cellMargin * 2) / ui.tileW;
   ui.tileStartF = 0.2;
-
-  ui.titleColour = color(0, 0, 100);  
-  ui.menuColour = color(0, 0, 100);
-  ui.bgColour = color(0, 0, 0);
-  ui.boardColour = color(0, 0, 50);
-  ui.cellColour = color(0, 0, 90);
-  ui.numColour = color(0);
   
   ui.titleTextSize = ui.tileW;
-  ui.numbersTextSize = ui.tileW / 20;
-  ui.numbersTextOffset = ui.numbersTextSize;
+  ui.numbersTextSize = ui.tileW / 2;
+  ui.numbersTextOffset = ui.numbersTextSize / 10;
   ui.scoreTextSize = ui.tileW / 6.5;
   ui.gameOverTextSize = ui.tileW / 2
+
+  ui.titleColour = color(171, 23, 83);
+  ui.menuColour = color(0, 0, 98);
+  ui.bgColour = color(216, 77, 17);
+  ui.windowColour = color(215, 76, 25)
+  ui.gameBoardColour = color(215, 78, 48);
+  ui.cellColour = color(215, 20, 91);
+  ui.numColour = color(213, 76, 8);
 }
 
 function initSettings() {
@@ -433,12 +447,12 @@ class Cell {
     this.newTileId;
     this.occupied = false;
     this.isCombining = false;
-    this.pos = createVector(ui.cellMargin + (ui.cellSpacing) * this.c, ui.cellMargin + (ui.cellSpacing) * this.r);
+    this.pos = createVector(ui.cellMargin + ui.cellSpacing * this.c, ui.cellMargin + ui.cellSpacing * this.r);
+    console.log("New cell created");
   }
   
   display() {
-    fill(ui.cellColour);
-    rect(this.pos.x, this.pos.y, ui.tileW, ui.tileW, ui.tileFillet);
+    rect(this.pos.x, this.pos.y, ui.tileW, ui.tileW, ui.tileCornerRadius);
   }
   
   empty() {
@@ -457,21 +471,21 @@ class Cell {
   }
   
   update() {
-    printId();
-    print("update() ");
-    print("exponent=" + this.exponent + ' ');
+    this.printId();
+    console.log("update() ");
+    console.log("exponent=" + this.exponent + ' ');
     if (this.isCombining) {
       tiles[this.newTileId].destroy();
-      print("destroyed tile " + this.newTileId + ' ');
-      poppingTileIds.append(this.tileId);
+      console.log("destroyed tile " + this.newTileId + ' ');
+      poppingTileIds.push(this.tileId);
       this.isCombining = false;
     }
     tiles[this.tileId].setExp(this.exponent);
-    println("tileId=" + this.tileId);
+    console.log("tileId=" + this.tileId);
   }
   
   printId() {
-    print("cells[" + this.c + "][" + this.r + "]::");
+    console.log("cells[" + this.c + "][" + this.r + "]::");
   }
 }
 
@@ -491,14 +505,15 @@ class Tile {
     this.targetPos = createVector(0, 0);
     this.vel = createVector(0, 0);
     this.scaleF;
-    this.textSize;
+    this.numTextSize;
+    console.log("Tile created");
   }
   
   create(c, r, exp) {
-    this.pos = createVector(ui.cellMargin + (ui.cellSpacing) * this.c + ui.tileW / 2, ui.cellMargin + (ui.cellSpacing) * this.r + ui.tileW / 2);
+    this.pos = createVector(ui.cellMargin + (ui.cellSpacing) * c + ui.tileW / 2, ui.cellMargin + (ui.cellSpacing) * r + ui.tileW / 2);
     this.targetPos = createVector(this.pos.x, this.pos.y);
     this.vel = createVector(0, 0);
-    setExp(exp);
+    this.setExp(exp);
     this.scaleF = ui.tileStartF;
     this.active = true;
   }
@@ -512,31 +527,32 @@ class Tile {
     translate(this.pos.x, this.pos.y);
     scale(this.scaleF);
     
-    fill(ui.tileColours[exponent-1]);
-    rect(-ui.tileW / 2, -ui.tileW / 2, ui.tileW, ui.tileW, ui.tileFillet);
+    fill(ui.tileColours[this.exponent-1]);
+    rect(-ui.tileW / 2, -ui.tileW / 2, ui.tileW, ui.tileW, ui.tileCornerRadius);
     
-    textSize(this.textSize);
+    textSize(this.numTextSize);
+    textAlign(CENTER, CENTER)
     fill(ui.numColour);
-    text(int(pow(2, this.exponent)), -2, this.textSize * 0.4);
+    text(int(pow(2, this.exponent)), -1, -this.numTextSize * 0.05);
     
     pop();
   }
   
   goTo(c, r) {
-    print("transferring tile " + this.id + " to " + this.c + " " + this.r + ' ');
+    console.log("transferring tile " + this.id + " to " + this.c + " " + this.r + ' ');
     this.targetPos.set(cells[c][r].pos).add(ui.tileW/2, ui.tileW/2);
     this.vel.set(this.targetPos).sub(this.pos).div(settings.moveFrames);
-    movingTileIds.append(this.id);
+    movingTileIds.push(this.id);
     cells[c][r].setTile(this.id);
   }
   
   setExp(exp) {
     this.exponent = exp;
     
-    textSize = ui.numbersTextSize;
+    this.numTextSize = ui.numbersTextSize;
     if (this.exponent > 6) {
-      let log = int(log(pow(2, this.exp)) / log(10)) + 1;
-      textSize -= ui.numbersFontOffset * (log - 1);
+      let numDigits = digits(pow(2, exp));
+      this.numTextSize -= ui.numbersTextOffset * (numDigits - 1);
     }
   }
   
@@ -551,4 +567,14 @@ class Tile {
   pop() {
     this.scaleF -= (ui.tilePopF - 1) / settings.tilePopFrames;
   }
+}
+
+function digits(num) {
+  let n = num;
+  let d = 0;
+  while (n >= 1) {
+    n *= 0.1;
+    d++;
+  }
+  return d;
 }
